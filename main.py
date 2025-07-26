b4c02b9ea2a1f0eeeaa5ef5340b87caff8a4d303
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-07-25 23:16:41 -0700

    .001 the finally got tkinter to work

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
-from asciimatics.screen import Screen
-from time import sleep
+import tkinter as tk
+from tkinter import ttk
 
-def main():
-    def start(screen):
-        start.clear()
-        screen.print_at('Hello! Welcome to Franklin Flash,my most recent way to use flashcards in the terminal. this is perfect wfor when you want to make studying about computers require using computers', 0, 0)
-        screen.refresh()
-        sleep(10)
+class Base_GUI:
+    def __init__(self):
+        self.root = tk.Tk()
 
-    Screen.wrapper(start)
+        self.root.geometry("800x400")
+        self.root.title("Franklin Flash")
 
-if __name__ == "__main__":
-    main()
+        self.label = tk.Label(self.root,text = "Welcome to the Franklin Flash! Where learning about computers gets better with computer!", font = ('Arial', 18))
+
+        self.label.pack(padx=20, pady =20)
+
+        self.textbox = tk.Text(self.root, height = 3, font = ('Arial', 16))
+        self.textbox.pack()
+
+        buttonframe = tk.Frame(self.root)
+        buttonframe.columnconfigure(0 , weight =1) 
+        buttonframe.columnconfigure(1 , weight =1) 
+
+        btn1 = tk.Button(buttonframe, text="Yes", font = ('Arial', 18))
+        btn1.grid(row=0 , column =0 ,sticky = tk.W + tk.E)
+
+
+        btn2 = tk.Button(buttonframe, text="No", font = ('Arial', 18))
+        btn2.grid(row=0 , column =1, sticky= tk.W + tk.E)
+        
+        buttonframe.pack(fill = 'x')
+
+        scorebtn = tk.Button(self.root, text = "Show Score!")
+        scorebtn.place(x=350 , y=300, heigh=100, width = 100)
+        
+        self.score = 0
+        
+        self.root.mainloop()
+
+    def modify_score():
+        if btn1.checkbutton == 1: 
+            self.score += 1
+    
+
+
+
+Base_GUI()
