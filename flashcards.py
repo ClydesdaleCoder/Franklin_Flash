826d0be05920f52ed7c30deb5b0b01c1dbb2fd41
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-07-26 15:09:18 -0700

    Revert ".005 Got start screen to work"
    This reverts commit ce32b871da40806e564629af73820d42604f7493.

--- flashcards.py
+++
@@ -1,7 +0,0 @@
-flashcards_data =[
-    {"question": "What is the capital of France?", "answer": "Paris"},
-    {"question": "What is 2 + 2?", "answer": "4"},
-    {"question": "What language is this app written in?", "answer": "Python"}
-]
-
-test_variable = "hello"
--- main.py
+++ main.py
@@ -1,43 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox
-from flashcards import flashcards_data  
 
 class Base_GUI:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.withdraw()
-
-        self.cards = flashcards_data
-        self.card_place = 0
-        self.current_card = self.cards[self.card_place]["question"]
 
         self.score = 0
-        #button Checks
-        self.check_startbutn =  tk.BooleanVar()
         self.check_scorebutn = tk.BooleanVar()
         self.check_yesbutn = tk.BooleanVar()
         self.check_nobutn = tk.BooleanVar()
         
-        #starter screen
-        self.splash_root = tk.Toplevel()
-        self.splash_root.title('Franklin Flash starting screen')
-        self.splash_root.label = tk.Label(self.splash_root, text="Welcome to the Franklin Flash! Where learning about computers gets better with computers!", font = ('Arial',18))
-        self.splash_root.geometry("800x400")
-        self.startbtn = tk.Button(self.splash_root, text= 'Get Started', font= ('Arial',18), command=self.handle_startclick)
-        self.startbtn.place(x=400 , y=200, heigh=100, width = 100)
-        
-        #Main app screen
         #size and name
         self.root.geometry("800x400")
         self.root.title("Franklin Flash")
 
         #texts within
-        self.label = tk.Label(self.root,text = "Look at the the card and mark yes if you know answer and no if you do not.", font = ('Arial', 18))
+        self.label = tk.Label(self.root,text = "Welcome to the Franklin Flash! Where learning about computers gets better with computer!", font = ('Arial', 18))
 
         self.label.pack(padx=20, pady =20)
 
         self.textbox = tk.Text(self.root, height = 3, font = ('Arial', 16))
-        self.textbox.insert("1.0", f"{self.current_card} ")
         self.textbox.pack()
 
         self.buttonframe = tk.Frame(self.root)
@@ -48,7 +30,8 @@ class Base_GUI:
         self.yesbutn.grid(row=0 , column =0 ,sticky = tk.W + tk.E)
 
 
-        self.nobutn = tk.Button(self.buttonframe, text="No", font = ('Arial', 18), command=self.handle_noclick)
+        self.nobutn = tk.Button(self.buttonframe, text="No", font = ('Arial', 18), command=self.check_nobutn)
+        self.nobutn.bind("<Button-1>", self.check_show_score)
         self.nobutn.grid(row=0 , column =1, sticky= tk.W + tk.E)
         
         self.buttonframe.pack(fill = 'x')
@@ -61,10 +44,9 @@ class Base_GUI:
         self.score_label = tk.Label(self.root, text = self.score , font=('Arial', 12))
         self.score_label.config(text=self.score)
         
-
+        
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.splash_root.mainloop()
-        self.root.mainloop()       
+        self.root.mainloop()
 
     def check_show_score(self, event=None):
         if self.check_scorebutn.get() == 0:
@@ -73,25 +55,15 @@ class Base_GUI:
              self.score_label.place_forget()
              print(self.textbox.get('1.0', tk.END))
 
-#click functionalities:        
+        
     def handle_yesclick(self,event=None):
         if self.check_yesbutn.get() == 0: 
                 self.score += 1
                 self.score_label.config(text=self.score)
-        self.card_place +=1
-        if self.card_place >= len(self.cards):
-             self.card_place = 0 
-        self.textbox.delete('1.0', tk.END)  
-        self.textbox.insert('1.0', self.cards[self.card_place]["question"]) 
 
     def handle_noclick(self,event=None):
          pass
-    
-    def handle_startclick(self, event=None):
-         self.splash_root.destroy()
-         self.root.deiconify()
 
-    
     def on_closing(self):
         if messagebox.askyesno(title = "Done Studying?", message="Franklin thinks you need to keep studying, are you sure?" ):
              self.root.destroy()
