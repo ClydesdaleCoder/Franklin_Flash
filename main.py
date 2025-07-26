dab000c260721f54ddac0478dc6e9e9205c8e6cf
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-07-26 12:33:40 -0700

    .003 Got the score to show and hide

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ class Base_GUI:
     def __init__(self):
         self.root = tk.Tk()
 
+        self.score = 0
+        self.checkbutton_value = tk.BooleanVar()
+        
+        #size and name
         self.root.geometry("800x400")
         self.root.title("Franklin Flash")
 
+        #texts within
         self.label = tk.Label(self.root,text = "Welcome to the Franklin Flash! Where learning about computers gets better with computer!", font = ('Arial', 18))
 
         self.label.pack(padx=20, pady =20)
@@ -28,32 +33,28 @@ class Base_GUI:
         
         self.buttonframe.pack(fill = 'x')
 
-        self.scorebtn = tk.Checkbutton(self.root, text = "Show Score!")
+#scoring funtionality/buttons
+        self.scorebtn = tk.Checkbutton(self.root, text = "Show Score!", variable=self.checkbutton_value)
         self.scorebtn.bind("<Button-1>", self.check_show_score)
         self.scorebtn.place(x=350 , y=300, heigh=100, width = 100)
         
-        self.score = 0
+        self.score_label = tk.Label(self.root, text = self.score , font=('Arial', 12))
+        
         
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.mainloop()
 
-    def check_show_score(self):
-        if self.check_state.get() == 0:
-              print(self.textbox.get('1.0', tk.END))
-        else: 
-            messagebox.showinfo(title='Score', message=self.textbox.get('1.0',tk.END) )
-            score_label = tk.Label(root, text = self.score , font=('Arial', 12))
-            score_label.place(x=350,y=375)
+    def check_show_score(self, event=None):
+        if self.checkbutton_value.get() == 0:
+            self.score_label.place(x=350,y=375)
+        elif self.checkbutton_value.get() == 1:
+             self.score_label.place_forget()
+             print(self.textbox.get('1.0', tk.END))
 
         
     def modify_score(self):
-        if btn1.checkbutton == 1: 
+        if self.btn1.checkbutton == 1: 
                 self.score += 1
-
-    def shortcut(self,event):
-        print (event.state)
-        print (event.keysym)
-
 
     def on_closing(self):
         if messagebox.askyesno(title = "Done Studying?", message="Franklin thinks you need to keep studying, are you sure?" ):
