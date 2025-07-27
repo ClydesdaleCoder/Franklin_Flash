9a96f340393cfd4a625a21dfc89e602ce6414963
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-07-26 18:27:10 -0700

    .008 Got flipping cards to work

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ class Base_GUI:
         self.cards = flashcards_data
         self.card_place = 0
         self.current_card = self.cards[self.card_place]["question"]
-
+        self.showing_question = True
         self.score = 0
+        
         #button Checks
         self.check_startbutn =  tk.BooleanVar()
         self.check_scorebutn = tk.BooleanVar()
         self.check_yesbutn = tk.BooleanVar()
         self.check_nobutn = tk.BooleanVar()
+        self.check_flipbutn = tk.BooleanVar()
         
         #starter screen
         self.splash_root = tk.Toplevel()
@@ -24,7 +26,7 @@ class Base_GUI:
         self.splash_root.label = tk.Label(self.splash_root, text="Welcome to the Franklin Flash! Where learning about computers gets better with computers!", font = ('Arial',18))
         self.splash_root.geometry("800x400")
         self.startbtn = tk.Button(self.splash_root, text= 'Get Started', font= ('Arial',18), command=self.handle_startclick)
-        self.startbtn.place(x=400 , y=200, heigh=100, width = 100)
+        self.startbtn.place(x=400 , y=200, height=100, width = 100)
         
         #Main app screen
         #size and name
@@ -44,12 +46,17 @@ class Base_GUI:
         self.buttonframe.columnconfigure(0 , weight =1) 
         self.buttonframe.columnconfigure(1 , weight =1) 
 
-        self.yesbutn = tk.Button(self.buttonframe, text="Yes", font = ('Arial', 18), command=self.handle_yesclick)
-        self.yesbutn.grid(row=0 , column =0 ,sticky = tk.W + tk.E)
+        self.flipbutn = tk.Button(self.buttonframe, text="Flip", font = ('Arial', 18), command=self.handle_flipclick)
+        self.flipbutn.grid(row=0 ,column =0,sticky = tk.W + tk.E)
 
+        self.flipbutn = tk.Button(self.buttonframe, text="Flip", font = ('Arial', 18), command=self.handle_flipclick)
+        self.flipbutn.grid(row=0 ,column =0,sticky = tk.W + tk.E)
+
+        self.yesbutn = tk.Button(self.buttonframe, text="Yes", font = ('Arial', 18), command=self.handle_yesclick)
+        self.yesbutn.grid(row=1 , column =0 ,sticky = tk.W + tk.E)
 
         self.nobutn = tk.Button(self.buttonframe, text="No", font = ('Arial', 18), command=self.handle_noclick)
-        self.nobutn.grid(row=0 , column =1, sticky= tk.W + tk.E)
+        self.nobutn.grid(row=1 , column =1, sticky= tk.W + tk.E)
         
         self.buttonframe.pack(fill = 'x')
 
@@ -81,17 +88,33 @@ class Base_GUI:
         self.card_place +=1
         if self.card_place >= len(self.cards):
              self.card_place = 0 
+        self.showing_question = True
         self.textbox.delete('1.0', tk.END)  
         self.textbox.insert('1.0', self.cards[self.card_place]["question"]) 
 
     def handle_noclick(self,event=None):
-         pass
+        self.card_place +=1
+        if self.card_place >= len(self.cards):
+            self.card_place = 0
+        self.showing_question = True 
+        self.textbox.delete('1.0', tk.END)  
+        self.textbox.insert('1.0', self.cards[self.card_place]["question"]) 
     
     def handle_startclick(self, event=None):
          self.splash_root.destroy()
          self.root.deiconify()
 
-    
+    def handle_flipclick(self,event=None):
+        if  self.showing_question:
+            self.textbox.delete('1.0', tk.END)  
+            self.textbox.insert('1.0', self.cards[self.card_place]["answer"])
+            self.showing_question = False 
+
+        else:
+            self.textbox.delete('1.0', tk.END)  
+            self.textbox.insert('1.0', self.cards[self.card_place]["question"]) 
+            self.showing_question= True
+
     def on_closing(self):
         if messagebox.askyesno(title = "Done Studying?", message="Franklin thinks you need to keep studying, are you sure?" ):
              self.root.destroy()
