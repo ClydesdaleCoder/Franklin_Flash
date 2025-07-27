32ba48854c2859ca8f40472ae4e310180f8316e7
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-07-26 19:51:52 -0700

    .009 added randomizer and more test cards

--- flashcards.py
+++ flashcards.py
@@ -1,7 +1,10 @@
 flashcards_data =[
     {"question": "What is the capital of France?", "answer": "Paris"},
     {"question": "What is 2 + 2?", "answer": "4"},
-    {"question": "What language is this app written in?", "answer": "Python"}
+    {"question": "What language is this app written in?", "answer": "Python"},
+    {"question": "What port is HTTPS?", "answer": "TCP 443"},
+    {"question": "What does BIOS stand for?", "answer": "Basic Input/Output System"},
+    {"question": "Who wrote the Linux Kernel?", "answer": "Linus Torvalds "},
+    {"question": "What does TCP stand for?", "answer": "Transmission Control Protocol"},
+    {"question": "What does UDP stand for?", "answer": "User Datagram Protocol"}
 ]
-
-test_variable = "hello"
--- main.py
+++ main.py
@@ -19,14 +19,15 @@ class Base_GUI:
         self.check_yesbutn = tk.BooleanVar()
         self.check_nobutn = tk.BooleanVar()
         self.check_flipbutn = tk.BooleanVar()
+        self.check_randomizerbutn = tk.BooleanVar()
         
         #starter screen
         self.splash_root = tk.Toplevel()
         self.splash_root.title('Franklin Flash starting screen')
         self.splash_root.label = tk.Label(self.splash_root, text="Welcome to the Franklin Flash! Where learning about computers gets better with computers!", font = ('Arial',18))
-        self.splash_root.geometry("800x400")
+        self.splash_root.geometry("400x400")
         self.startbtn = tk.Button(self.splash_root, text= 'Get Started', font= ('Arial',18), command=self.handle_startclick)
-        self.startbtn.place(x=400 , y=200, height=100, width = 100)
+        self.startbtn.place(x=150 , y=150, height=100, width = 100)
         
         #Main app screen
         #size and name
@@ -47,38 +48,57 @@ class Base_GUI:
         self.buttonframe.columnconfigure(1 , weight =1) 
 
         self.flipbutn = tk.Button(self.buttonframe, text="Flip", font = ('Arial', 18), command=self.handle_flipclick)
-        self.flipbutn.grid(row=0 ,column =0,sticky = tk.W + tk.E)
-
-        self.flipbutn = tk.Button(self.buttonframe, text="Flip", font = ('Arial', 18), command=self.handle_flipclick)
-        self.flipbutn.grid(row=0 ,column =0,sticky = tk.W + tk.E)
+        self.flipbutn.grid(row=0 ,column =0, sticky = tk.W + tk.E ,columnspan=2 , padx=10 ,pady=5)
 
         self.yesbutn = tk.Button(self.buttonframe, text="Yes", font = ('Arial', 18), command=self.handle_yesclick)
-        self.yesbutn.grid(row=1 , column =0 ,sticky = tk.W + tk.E)
+        self.yesbutn.grid(row=1 , column =0 ,sticky = tk.W + tk.E, padx=10 ,pady=5)
 
         self.nobutn = tk.Button(self.buttonframe, text="No", font = ('Arial', 18), command=self.handle_noclick)
-        self.nobutn.grid(row=1 , column =1, sticky= tk.W + tk.E)
+        self.nobutn.grid(row=1 , column =1, sticky= tk.W + tk.E, padx=10 ,pady=5)
         
         self.buttonframe.pack(fill = 'x')
 
-#scoring funtionality/buttons
+#checkbox funtionality/buttons
         self.scorebtn = tk.Checkbutton(self.root, text = "Show Score!", variable=self.check_scorebutn)
         self.scorebtn.bind("<Button-1>", self.check_show_score)
-        self.scorebtn.place(x=350 , y=300, heigh=100, width = 100)
+        self.scorebtn.place(x=200 , y=300, heigh=100, width = 100)
         
         self.score_label = tk.Label(self.root, text = self.score , font=('Arial', 12))
         self.score_label.config(text=self.score)
         
+        self.randomizerbtn = tk.Checkbutton(self.root, text = "Randomize cards", variable=self.check_randomization)
+        self.randomizerbtn.bind("<Button-1>", self.check_randomization)
+        self.randomizerbtn.place(x=600 , y=300, heigh=100, width = 100)
+
+
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.splash_root.mainloop()
         self.root.mainloop()       
-
+#Checkboxe functions
     def check_show_score(self, event=None):
         if self.check_scorebutn.get() == 0:
             self.score_label.place(x=350,y=375)
         elif self.check_scorebutn.get() == 1:
              self.score_label.place_forget()
              print(self.textbox.get('1.0', tk.END))
+    
+    def check_randomization(self, event=None):
+        if  self.check_scorebutn.get() == 0:
+            import random
+            random.shuffle(self.cards)
+            self.card_place = 0
+            self.showing_question = True
+            self.textbox.delete('1.0', tk.END)
+            self.textbox.insert('1.0', self.cards[self.card_place]["question"])
+        
+        else:
+            self.card_place +=1
+            if self.card_place >= len(self.cards):
+                self.card_place = 0 
+            self.showing_question = True
+            self.textbox.delete('1.0', tk.END)  
+            self.textbox.insert('1.0', self.cards[self.card_place]["question"]) 
 
 #click functionalities:        
     def handle_yesclick(self,event=None):
@@ -117,8 +137,7 @@ class Base_GUI:
 
     def on_closing(self):
         if messagebox.askyesno(title = "Done Studying?", message="Franklin thinks you need to keep studying, are you sure?" ):
-             self.root.destroy()
-
-        
+            self.root.destroy()
+            self.splash_root.destroy()
 
 Base_GUI()
